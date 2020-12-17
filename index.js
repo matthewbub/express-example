91d26ba67ca7cd71dc41425aacc4970f622440ba
hi-matbub <6matbub@gmail.com>
2020-12-16 17:03:28 -0800

    fix: api path matches button innerHTML

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const home = path.resolve(__dirname, './index.html');
 // Here we are sending a static file index.html
 app.get('/', (req, res) => res.sendFile(home))
 
-app.get('/api', (req, res) => res.json('Hello from express!'))
+app.get('/api/hello-world', (req, res) => res.json('Hello from express!'))
 
-
+// app.post('/api')
 // .listen() is what runs our server
 app.listen(3000, () => console.log('Listening on localhost:3000'))
