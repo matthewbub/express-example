4bfeedfe817172f02480c5c00693554e57b920a2
hi-matbub <6matbub@gmail.com>
2020-12-16 17:47:48 -0800

    feat: handling POST route

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const path = require('path');
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 
 const home = path.resolve(__dirname, './index.html');
 // console.log(home)
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 // We generally start with the base url
@@ -10,6 +16,9 @@ const home = path.resolve(__dirname, './index.html');
 app.get('/', (req, res) => res.sendFile(home))
 
 app.get('/api/hello-world', (req, res) => res.json('Hello from express!'))
+
+app.post('/api/user-input', (req, res) => res.json(req.body));
+
 
 // app.post('/api')
 // .listen() is what runs our server
